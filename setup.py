bda449dad018e3659bac78aa3c059d363a9b6e80
Kendall Park <darkidder@gmail.com>
2019-11-03 17:00:51 -0600

    chore: configure as Python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='panaugment',
+      version='0.0.1',
+      description='Augmentation caching.',
+      url='http://github.com/KendallPark/panaugment',
+      author='Kendall Park',
+      author_email='darkidder@gmail.com',
+      license='MIT',
+      packages=['panaugment'])
